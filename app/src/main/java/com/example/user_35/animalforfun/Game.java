b9069689bb84203cea95d79af74d71508bd91175
munow24686 <kurumi.erika.cure.marine@gmail.com>
2017-03-23 11:07:24 +0700

    Design Game UI Complete

--- app/src/main/java/com/example/user_35/animalforfun/Game.java
+++ app/src/main/java/com/example/user_35/animalforfun/Game.java
@@ -1,8 +1,17 @@
 package com.example.user_35.animalforfun;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by USER-35 on 3/23/2017.
  */
 
-public class Game {
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
 }
